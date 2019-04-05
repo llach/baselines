4533488173593b8842e567167741273295b25507
llach <llach@techfak.uni-bielefeld.de>
2019-04-05 10:06:32 +0200

    removed unnecessary print

--- baselines/ppo2/ppo2.py
+++ baselines/ppo2/ppo2.py
@@ -191,7 +191,7 @@ def learn(*, network, env, total_timesteps, eval_env = None, seed=None, nsteps=2
         obs, returns, masks, actions, values, neglogpacs, states, epinfos = runner.run() #pylint: disable=E0632
         if eval_env is not None:
             eval_obs, eval_returns, eval_masks, eval_actions, eval_values, eval_neglogpacs, eval_states, eval_epinfos = eval_runner.run() #pylint: disable=E0632
-        print(states)
+
 
         """ This is for observation debugging. Uncomment with caution. """
         # import matplotlib.pyplot as plt
