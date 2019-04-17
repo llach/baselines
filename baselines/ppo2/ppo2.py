f6105ee4575b64fe6769b3ad749c8fd53f9dd5a9
llach <llach@techfak.uni-bielefeld.de>
2019-04-18 00:56:07 +0200

    ppo2: printing violation

--- baselines/ppo2/ppo2.py
+++ baselines/ppo2/ppo2.py
@@ -331,7 +331,7 @@ def learn(*, network, env, total_timesteps, eval_env = None, seed=None, nsteps=2
                         pi_kl = res[-2]
                         mblossvals.append(res)
                     if (pi_kl > 1.5 * target_kl) and early_stop:
-                        logger.log('KL contraint violated.')
+                        logger.log(f'KL {pi_kl} violates constraint {1.5*target_kl}.')
                         stop = True
                         break
                 if early_stop and stop:
